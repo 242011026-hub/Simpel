031661261b911270cbb13f3fd82036365a91684b
242011026-hub <242011026@asia.ac.id>
2026-06-13 10:15:35 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SuratController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get('/surat', [SuratController::class, 'daftarSurat']);
+Route::resource('surat', SuratController::class );
